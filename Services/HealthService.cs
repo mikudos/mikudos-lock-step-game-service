a7e2fea12a206cc0f29cd975fc123341542a0764
guanyu <yue.guanyu@hotmail.com>
2020-06-10 16:35:15 +0800

    add health check

---
+++ Services/HealthService.cs
@@ -0,0 +1,22 @@
+using System.Threading.Tasks;
+using Grpc.Core;
+using Grpc.Health.V1;
+using System.Timers;
+
+namespace mikudos_lock_step_game_service
+{
+    public class HealthImpl : Health.HealthBase
+    {
+        public override Task<HealthCheckResponse> Check(HealthCheckRequest request, ServerCallContext context)
+        {
+            var servcieName = request.Service;
+            return Task.FromResult(new HealthCheckResponse { Status = HealthCheckResponse.Types.ServingStatus.Serving });
+        }
+
+        public override async Task Watch(HealthCheckRequest request, IServerStreamWriter<HealthCheckResponse> responseStream, ServerCallContext context)
+        {
+            var servcieName = request.Service;
+            await responseStream.WriteAsync((new HealthCheckResponse { Status = HealthCheckResponse.Types.ServingStatus.Serving }));
+        }
+    }
+}
